f69451825c4c7820da667ea4508de200a41f34c2
Anton Isaev <aisaev@intetics.com>
2021-03-24 11:00:05 +0200

    auth module generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, TasksModule],
+  imports: [DatabaseModule, TasksModule, AuthModule],
 })
 export class AppModule {}
