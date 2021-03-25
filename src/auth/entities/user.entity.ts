75dc0810bddbaba92bcd54fa84b7be6a73b20d34
Anton Isaev <aisaev@intetics.com>
2021-03-25 09:24:52 +0200

    changed user - task relation configuration

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -2,11 +2,9 @@ import {
   BaseEntity,
   Column,
   Entity,
-  OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
-import { Task } from 'src/tasks/entities/task.entity';
 import * as bcrypt from 'bcrypt';
 
 @Entity()
@@ -23,9 +21,6 @@ export class User extends BaseEntity {
 
   @Column()
   salt: string;
-
-  @OneToMany(() => Task, (task) => task.user, { eager: true })
-  tasks: Task[];
 
   async validatePassword(password: string): Promise<boolean> {
     const hash = await bcrypt.hash(password, this.salt);
